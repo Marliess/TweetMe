e3d2eed3d6cd9cf186ed18e53f8a42964884a86e
gjghendriks <gjghendriks@gmail.com>
2015-03-12 18:00:47 +0100

    made a setup for the crawler class

---
+++ crawler.py
@@ -0,0 +1,31 @@
+'''
+Authors:
+Gijs Hendriks - s2410540
+Sil de Graaf -
+Marlies Quekel - 
+
+Supervisor:
+GJ van Noord
+'''
+
+import tweet
+
+class crawler():
+    '''
+    A class to 'walk' through all the tweets and compare them
+    '''
+
+    def __init__(self):
+        pass
+
+    def randomCouple(self):
+        '''
+        returns a tuple of two random ryhming tweets
+        '''
+        pass
+
+    def giveNext(self, tweet):
+        '''
+        return a tweet that ryhmes with given tweet and that is also not been returned yet
+        '''
+        pass

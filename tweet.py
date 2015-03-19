6dff354427e2363667fc5ad5c48ed04c117bdc62
Marliess <liezq@live.nl>
2015-03-19 11:59:12 +0100

    Tried something new with ryhmes

--- tweet.py
+++ tweet.py
@@ -23,12 +23,16 @@ class tweet():
         #self.date = date
         self.length = len(text)
         words = text.split(' ')
-        self.ryhmeWord = word.word(words[len(words)-1])
+        self.ryhmeWord = word.word(words[len(words)-1])     # RyhmeWord is 'afspraken'
+        tweet.ryhmeWord = word.word(words[len(words) -1])   # klopt nog niet, is nu ook 'afspraken'
+        self.ryhmes(tweet)
         
     def ryhmes(self, tweet):
         if self.ryhmeWord.phonetic == tweet.ryhmeWord.phonetic:
+            print("{} == {} so this ryhmes").format(self.ryhmeWord.phonetic, tweet.ryhmeWord.phonetic)
             return 1
         else:
+            print("This doesn't ryhme")
             return 0
     
     def __str__(self):
@@ -37,9 +41,3 @@ class tweet():
 
 t = tweet("@aafkevultink zal nog wel een aantal jaren duren voor nieuwe afspraken")
 print(t)
-
-
-
-"""
-t = open(tweet("woorden.txt"))
-print(t) """
